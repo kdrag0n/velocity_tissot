f94630bb6411837624e1b6114ce9fea9d6626c1a
Iliyan Malchev <malchev@google.com>
2015-05-20 20:04:52 -0700

    BACKPORT: PM: wakeup_reasons: disable wakeup-reason deduction by default
    Introduce a config item, CONFIG_DEDUCE_WAKEUP_REASONS, disabled by default.
    Make CONFIG_PARTUALRESUME select it.
    
    (cherry picked from commit 753b5405eedf67357952e3a10cb3fb1392a3972b)
    Change-Id: I7d831ff0a9dfe0a504824f4bc65ba55c4d92546b
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- include/linux/wakeup_reason.h
+++ include/linux/wakeup_reason.h
@@ -99,12 +99,7 @@ get_wakeup_reasons(unsigned long timeout, struct list_head *unfinished);
 void log_base_wakeup_reason(int irq);
 void clear_wakeup_reasons(void);
 void log_wakeup_reason(int irq);
-int check_wakeup_reason(int irq);
-
-#ifdef CONFIG_SUSPEND
 void log_suspend_abort_reason(const char *fmt, ...);
-#else
-static inline void log_suspend_abort_reason(const char *fmt, ...) { }
-#endif
+int check_wakeup_reason(int irq);
 
 #endif /* _LINUX_WAKEUP_REASON_H */
