9afc24fd957b8634e635cec4824addd73203116b
Martichou <martichou.andre@gmail.com>
2018-01-12 23:21:09 +0000

    Revert "irq: Add wakeup reason logging"
    This reverts commit d3030cbb3dbb252f229bfeac669a8f3829c80905.

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -25,7 +25,6 @@
 #include <linux/percpu.h>
 #include <linux/slab.h>
 #include <linux/module.h>
-#include <linux/wakeup_reason.h>
 
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/syscore_ops.h>
@@ -387,14 +386,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
-		log_base_wakeup_reason(irq);
-#ifdef CONFIG_HTC_POWER_DEBUG
-		if (EE0_KRAIT_HLOS_SPMI_PERIPH_IRQ != irq )
-			if (TLMM_MSM_SUMMARY_IRQ != irq )
-				pr_info("[WAKEUP] Resume caused by gic-%d\n", irq);
-#endif
-
-		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		log_wakeup_reason(irq);
 	}
 }
 
