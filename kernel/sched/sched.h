5d3657156cbab59587886bc8011e4f9567967c6b
kdrag0n <dragon@khronodragon.com>
2018-04-14 17:45:37 -0700

    sched: fix missing ENQUEUE_WAKING

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1230,9 +1230,9 @@ static const u32 prio_to_wmult[40] = {
 
 #define ENQUEUE_HEAD		0x08
 #ifdef CONFIG_SMP
-#define ENQUEUE_MIGRATED	0x20
+#define ENQUEUE_WAKING		0x20
 #else
-#define ENQUEUE_MIGRATED	0x00
+#define ENQUEUE_WAKING		0x00
 #endif
 #define ENQUEUE_REPLENISH	0x08
 #define ENQUEUE_RESTORE		0x10
