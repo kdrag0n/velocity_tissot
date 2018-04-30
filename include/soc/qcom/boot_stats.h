96f0523fd810c7f5a8138e2713aa37c46bd1b579
Tyler Nijmeh <tylernij@gmail.com>
2018-04-29 21:30:06 -0700

    defconfig: disabled some log drivers
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/soc/qcom/boot_stats.h
+++ include/soc/qcom/boot_stats.h
@@ -33,7 +33,7 @@ int boot_stats_exit(void);
 unsigned long long int msm_timer_get_sclk_ticks(void);
 #else
 static inline int boot_stats_init(void) { return 0; }
-unsigned long long int msm_timer_get_sclk_ticks(void) { return 0; }
+//unsigned long long int msm_timer_get_sclk_ticks(void) { return 0; }
 #endif
 
 #ifdef CONFIG_MSM_BOOT_TIME_MARKER
