718952c83bd68947175cd3793a18882702d237ff
Tyler Nijmeh <tylernij@gmail.com>
2018-05-24 16:48:53 -0700

    devfreq: microfreq: more ramping on touch
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/devfreq/governor_microfreq.c
+++ drivers/devfreq/governor_microfreq.c
@@ -128,7 +128,7 @@ static int devfreq_microfreq_func(struct devfreq *df,
 	/* If input, ramp */
 	if (gpu_boost_pending) {	
 		gpu_boost_pending = false;
-		b = div_u64(b, (RAMP_MULTIPLIER - DERAMP_MULTIPLIER));
+		b = div_u64(b, (RAMP_MULTIPLIER - DERAMP_MULTIPLIER * 2));
 	} else {
 		b = div_u64(b, (RAMP_MULTIPLIER - DERAMP_MULTIPLIER / 2));
 	}
