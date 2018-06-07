165df4a4bd394abd2807df6ebbd966814e9592a0
kdrag0n <dragon@khronodragon.com>
2018-06-06 17:44:38 -0700

    Revert "Force disallow L2PC during wakeup from SLUMBER"
    This reverts commit 2a653aac86384117a38b19e3235e8a431e0d8c76.

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -2180,7 +2180,9 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 	kgsl_property_read_u32(device, "qcom,l2pc-cpu-mask",
 			&pwr->l2pc_cpus_mask);
 
-	pwr->l2pc_update_queue = true;
+	pwr->l2pc_update_queue = of_property_read_bool(
+				device->pdev->dev.of_node,
+				"qcom,l2pc-update-queue");
 
 	pm_runtime_enable(&pdev->dev);
 
