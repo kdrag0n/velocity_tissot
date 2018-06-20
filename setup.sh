e9068044bcb2990c116bdaaee68ee040cd5fcd4a
kdrag0n <dragon@khronodragon.com>
2018-06-19 21:14:50 -0700

    setup: improve compiler version string
    The AOSP clang toolchain version string doesn't come out well when
    processed like the DragonTC version string. This new filter by
    nathanchance works better.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- setup.sh
+++ setup.sh
@@ -35,7 +35,8 @@ export KBUILD_BUILD_USER=velocity
 export KBUILD_BUILD_HOST=kernel
 export TOOL_CHAIN_PATH=$tc_gcc/bin/aarch64-linux-gnu-
 export CLANG_TCHAIN=$CLANG_PREBUILT_BIN/clang
-export CLANG_VERSION="$(${CLANG_TCHAIN} --version|head -n1|cut -d'(' -f1,4|sed -e 's/[[:space:]]*$//')"
+#export CLANG_VERSION="$(${CLANG_TCHAIN} --version|head -n1|cut -d'(' -f1,4|sed -e 's/[[:space:]]*$//'|sed 's/\(.+\)//')"
+export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export REAL_COMPILER=clang
 export DRAGONTC=$dragontc
 
