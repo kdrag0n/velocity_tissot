4e6374e5600a51af08f8e4b16aa0e51f481e9afe
kdrag0n <dragon@khronodragon.com>
2018-07-02 19:09:45 -0700

    flasher: fix SELinux policy for stock Wi-Fi binding
    This also fixed the Spectrum initialization script.
    
    For future use, an SEPolicy allow generator was added in scripts/ that
    takes a file containing avc denial log lines as the first and only
    argument.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- flasher/ramdisk-patch/init.late.sh
+++ flasher/ramdisk-patch/init.late.sh
@@ -1,14 +1,13 @@
 #!/system/bin/sh
 # VELOCITY KERNEL
-# Early initialization script
+# Late initialization script
 
-echo START >> /dev/abcdef
 # Wi-Fi module
 if [ -f /system/vendor/lib/modules/pronto/pronto_wlan.ko ]; then # stock
     cp /pronto_wlan.ko /dev/.pronto_wlan.ko
+    chmod 644 /dev/.pronto_wlan.ko
     chcon u:object_r:vendor_file:s0 /dev/.pronto_wlan.ko
     mount --bind /dev/.pronto_wlan.ko /system/vendor/lib/modules/pronto/pronto_wlan.ko
 else # custom
     insmod /pronto_wlan.ko
 fi
-echo DONE >> /dev/abcdef
